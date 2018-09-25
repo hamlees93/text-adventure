37db913b11c9321dbad5bfbefdd2b22e17b09083
Hamish Lees <hamishlees6@gmail.com>
2018-09-25 16:24:59 +1000

    refactore main.rb

--- main.rb
+++ main.rb
@@ -1,100 +1,21 @@
-
-puts "You have decided to go to broadway mall shopping"
-sleep(1)
-puts "What kid of food would you like to have for lunch"
-sleep(1)
-puts "Options: Healthy   Unhealthy"
-
-response = gets.chomp.downcase
-    if response == "healthy"
-        puts "The healthy options are: Bunme   Boostjuice"
-
-        response = gets.chomp.downcase
-        if response == "bunme"
-            sleep(1)
-            puts "Great choice, you spend $6.50 on a banh mi"
-        else 
-            sleep(1)
-            puts "Great choice, you spend $5 on a fruit smoothie"
-        end
-    else 
-        puts "The unhealthy options are: Eat Istanbul   Din tai fung"
-        response= gets.chomp.downcase
-        if response == "eat istanbul"
-            sleep(1)
-            puts "Great choice, you spend $13.50 on a large HSP. You feel sick afterwards though..."
-        else 
-            sleep(1)
-            puts "Great choice, you spend $5 on a xiao long bao. Enjoy the MSG!!!"
-        end 
-    end
-       
-end
-
-
-
-
-
-##### Path - Nearby Cafes #####
-
-# Path: Nearby cafe2
-def cafe2mountain
-  system("clear")
-  puts "Mountain Street"
-end
-
-def cafe2bay
-  system("clear")
-  puts "Bay Street"
-end
-
-# Path: Nearby cafe1
-def cafe1
-  cafe1_flag = false
-  puts "You decided you grab a quick lunch nearby..."
-  while cafe1_flag == false
-    puts "Which street do you you want to check out the cafes on?"
-    puts "Options: Bay or Mountain"
-    cafe1 = gets.chomp!
-
-    if cafe1 == "bay"
-      cafe2bay
-      cafe1_flag = true
-    elsif cafe1 == "mountain"
-      cafe2mountain
-      cafe1_flag = true
-    else
-      system("clear")
-      puts "Ummm... are you lost?"
+require_relative 'broadway'
+require_relative 'nearby'
+require_relative 'deal'
+require_relative 'classroom'
 
 def lunch_hunt
-    system("clear")
-    puts "Its is 12:59pm at Coder Academy. You start having difficulty understanding what Bianca is saying... your stomach is growling.."
-    puts "Tick, Tock - 1:00pm."
-    puts "Boom its lunch break, what do you do?"
-    puts "Options: nearby, deal"
-    pathOption = gets.chomp!
-  
-    if pathOption == "nearby"
-
-      cafe1
-    elsif pathOption == "deal"
-      traffic_ligths
-    else
-      puts "Thats not an option (lunch_hunt method)"
-    end
+  system("clear")
+  puts "Its is 12:59pm at Coder Academy. You start having difficulty understanding what Bianca is saying... your stomach is growling.."
+  puts "Tick, Tock - 1:00pm."
+  puts "Boom its lunch break, what do you do?"
+  puts "Options: nearby, deal"
+  pathOption = gets.chomp.downcase
+  pathOption == "nearby" ? cafe1 : (pathOption == "deal" ? traffic_ligths : (pathOption == "classroom" ? classroom : (pathOption == "broadway" ? broadway : invalid_option)))
 end
-  
-lunch_hunt
-      
-      puts "Recalling the health benefits of fasting that you read last night, you decide to stay in for lunch.\n You try to try the git initialisation example from earlier.\n"
-# more options
-puts "You fire up Terminal. Type exactly what you enter into the Terminal. Otherwise, type help to ask someone to help or type exit to give up."
 
-userinput = gets.downcase.chomp
-if userinput = "exit",
-  puts "You close your laptop. 'I'll do this later', you say to yourself.\n\nYou never end up doing it later.\n\nFast forwarding to your next assessment, you stare at your empty Github repo wondering what went wrong. Future you hates past you with a passion."
-elsif userinput = "help",
-  puts 'You see Bianca walking out of the room. Flagging her down, you ask her to take a look and help you with git. Walking over, she looks at your screen. Sighing, she shakes her head and says: "try git init".\n'
-else userinput = "git init"
-  puts 'You type "git init". This initialised your local repo. Success!'
+def invalid_option 
+  puts "Invalid Option. Please try agin"
+  sleep(2)
+end
+
+lunch_hunt
