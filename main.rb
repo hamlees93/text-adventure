bb53d78f2c23d0c3f1248de1e880d98490e7941b
Allan Gao <allangao95@gmail.com>
2018-09-25 15:57:50 +1000

    refactored broadway

--- main.rb
+++ main.rb
@@ -1,38 +1,72 @@
+def healthy
+    sleep(2)
+    puts 'Banhmi'
+end
 
-puts "You have decided to go to broadway mall shopping"
-sleep(1)
-puts "What kid of food would you like to have for lunch"
-sleep(1)
-puts "Options: Healthy   Unhealthy"
+def unhealthy 
+    sleep(2)
+    puts 'Large Snack Pack'
+end
 
-response = gets.chomp.downcase
-
-
+def broadway
+    option = false
+  puts "You decided to go to broadway for lunch"
+  while option == false
+    sleep(2)
+    puts "Do you want to eat healthy or unhealthy"
+    puts "Options: Healthy   Unhealthy"
+    response = gets.chomp.downcase
 
     if response == "healthy"
-        puts "The healthy options are: Bunme   Boostjuice"
-
-        response = gets.chomp.downcase
-        if response == "bunme"
-            sleep(1)
-            puts "Great choice, you spend $6.50 on a banh mi"
-        else 
-            sleep(1)
-            puts "Great choice, you spend $5 on a fruit smoothie"
-        end
-    else 
-        puts "The unhealthy options are: Eat Istanbul   Din tai fung"
-        response= gets.chomp.downcase
-        if response == "eat istanbul"
-            sleep(1)
-            puts "Great choice, you spend $13.50 on a large HSP. You feel sick afterwards though..."
-        else 
-            sleep(1)
-            puts "Great choice, you spend $5 on a xiao long bao. Enjoy the MSG!!!"
-        end 
+      healthy
+      option = true
+    elsif response == "unhealthy"
+      unhealthy
+      option = true
+    else
+      system("clear")
+      puts "Invalid option"
+      broadway
     end
-       
+  end
 end
+
+
+
+# puts "You have decided to go to broadway mall shopping"
+# sleep(1)
+# puts "What kid of food would you like to have for lunch"
+# sleep(1)
+# puts "Options: Healthy   Unhealthy"
+
+# response = gets.chomp.downcase
+
+
+
+#     if response == "healthy"
+#         puts "The healthy options are: Bunme   Boostjuice"
+
+#         response = gets.chomp.downcase
+#         if response == "bunme"
+#             sleep(1)
+#             puts "Great choice, you spend $6.50 on a banh mi"
+#         else 
+#             sleep(1)
+#             puts "Great choice, you spend $5 on a fruit smoothie"
+#         end
+#     else 
+#         puts "The unhealthy options are: Eat Istanbul   Din tai fung"
+#         response= gets.chomp.downcase
+#         if response == "eat istanbul"
+#             sleep(1)
+#             puts "Great choice, you spend $13.50 on a large HSP. You feel sick afterwards though..."
+#         else 
+#             sleep(1)
+#             puts "Great choice, you spend $5 on a xiao long bao. Enjoy the MSG!!!"
+#         end 
+#     end
+       
+# end
 
 
 
